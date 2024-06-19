c28bb86bfd8f486e8b83be70f9445fb0356e604c
Даниил <danilka-shubnikov@mail.ru>
2024-06-20 01:22:50 +0400

    first commit

--- serverr.js
+++ serverr.js
@@ -19,39 +19,42 @@ const messages = []; // Массив для хранения сообщений
 const users = {}; // Объект для хранения пользователей и их имен
 
 let index = 1;
+try {
+  io.on("connection", (socket) => {
+    console.log("Новый пользователь подключился:", socket.id);
 
-io.on("connection", (socket) => {
-  console.log("Новый пользователь подключился:", socket.id);
+    // Запрос имени пользователя
+    socket.emit("request username");
 
-  // Запрос имени пользователя
-  socket.emit("request username");
+    socket.on("username", (username) => {
+      users[socket.id] = username; // Сохранение имени пользователя
+      username = !username ? "user" + index : username;
+      index++;
+      console.log("Имя пользователя:", username, " (ID:", socket.id, ")");
 
-  socket.on("username", (username) => {
-    users[socket.id] = username; // Сохранение имени пользователя
-    username = !username ? "user" + index : username;
-    index++;
-    console.log("Имя пользователя:", username, " (ID:", socket.id, ")");
+      // Отправка истории сообщений
+      socket.emit("chat history", messages);
 
-    // Отправка истории сообщений
-    socket.emit("chat history", messages);
+      // Отправка сообщения о подключении
+      socket.emit("user joined", username); // Сообщение всем, кроме нового пользователя
+      socket.broadcast.emit("user joined", username); // Сообщение всем, кроме нового пользователя
 
-    // Отправка сообщения о подключении
-    socket.emit("user joined", username); // Сообщение всем, кроме нового пользователя
-    socket.broadcast.emit("user joined", username); // Сообщение всем, кроме нового пользователя
+      socket.on("chat message", (msg) => {
+        console.log("Сообщение:", msg);
+        messages.push({ username: username, message: msg }); // Добавление сообщения с именем
+        io.emit("chat message", { username: username, message: msg });
+      });
 
-    socket.on("chat message", (msg) => {
-      console.log("Сообщение:", msg);
-      messages.push({ username: username, message: msg }); // Добавление сообщения с именем
-      io.emit("chat message", { username: username, message: msg });
-    });
-
-    socket.on("disconnect", () => {
-      console.log("Пользователь отключился:", socket.id);
-      delete users[socket.id]; // Удаление пользователя из списка
-      socket.broadcast.emit("user left", username); // Сообщение всем о выходе
+      socket.on("disconnect", () => {
+        console.log("Пользователь отключился:", socket.id);
+        delete users[socket.id]; // Удаление пользователя из списка
+        socket.broadcast.emit("user left", username); // Сообщение всем о выходе
+      });
     });
   });
-});
+} catch (err) {
+  console.log(err);
+}
 
 http.listen(8080, () => {
   console.log("Сервер запущен на http://localhost:8080");
